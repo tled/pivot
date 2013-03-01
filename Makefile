156faad9ec632747897607d8c2f66e93a21357b0
T. Led <tled@ledderboge.de>
2013-03-01 17:22:23 +0100

    pivot
    Squashed commit of the working branch

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 
-
-.PHONY: all clean
+.PHONY: all clean debug
 
 all: pivot
 
 pivot: pivot.c
-	gcc -g -lXrandr -o pivot pivot.c
+	gcc -Wall -O2 -lXrandr -o pivot pivot.c
+
+debug:
+	gcc -Wall -DDEBUG -g -lXrandr -o pivot pivot.c
 
 clean:
 	rm -f pivot
--- pivot.c
+++ pivot.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -9,32 +8,122 @@
 #include <X11/Xatom.h>
 #include <X11/extensions/Xrandr.h>
 
-static Display *display;
-static Window root_win;
-static int screen = -1;
+#ifdef DEBUG
+	#define debug(...) printf(__VA_ARGS__)
+#else
+	#define debug
+#endif
 
+//see Rotations/Reflections in man Xrandr
+#define normal 1
+#define left 1<<1
+#define inverted 1<<2
+#define right 1<<3
+
+
+struct _XHead {
+	Display *display;
+	Window root;
+	int screen;
+};
+
+struct _XRR {
+	XRRScreenConfiguration *screen_config;
+	SizeID size;
+	Rotation rotation;
+};
+
+struct _options {
+	unsigned int verbose:1;
+	unsigned int force:1;
+};
+
+
+typedef struct _XHead XHead;
+typedef struct _XRR XRR;
+typedef struct _options options;
+
+void help() {
+	printf("pivot: switching between two alternating  screen rotations\n");
+	printf("usage: pivot [options] [ rotation_1 [ rotation_2 ]]\n");
+	printf("  -h, --help     prints help\n");
+	printf("  -d, --display  display\n");
+	printf("  -f, --force    force switching\n");
+	printf("\n");
+	printf("rotations: left, right, normal, inverted\n");
+	printf("  default: left, normal\n");
+	printf("\n");
+	printf("examples:\n");
+	printf("  pivot -d :1 -v right\n");
+	printf("  pivot left normal\n");
+	printf("\n");
+}
 
 int main(int argc, char **argv) {
+	XHead xhead;
+	XRR xrr;
+	char *disp_name = NULL;
+	Rotation rotations = 0;
+	int i;
+	options opts;
+	opts.verbose = 0;
+	opts.force   = 0;
+	int r_opts  = 2;
 
-	short current_rate;
-	Rotation current_rotation;
-	int size = -1;
-	XRRScreenConfiguration *screen_config;
 
-	char *display_name = NULL;
+	for (i=1; i < argc; i++) {
+		if ( !strcmp(argv[i],"-h") || !strcmp(argv[i],"--help") ) {
+			help();
+			return 0;
+		}
+		if ( (!strcmp(argv[i],"-d") || !strcmp(argv[i],"--display")) && (++i < argc)) {
+			disp_name = argv[i];
+			continue;
+		}
+		if ( !strcmp(argv[i],"-v") || !strcmp(argv[i],"--verbose") ) {
+			opts.verbose = 1;
+			continue;
+		}
+		if ( !strcmp(argv[i],"-f") || !strcmp(argv[i],"--force") ) {
+			opts.force = 1;
+			continue;
+		}
+		if (r_opts > 0) {
+			if (!strcmp(argv[i],"normal"))   { rotations |= normal;   r_opts--;}
+			if (!strcmp(argv[i],"left"))     { rotations |= left;     r_opts--;}
+			if (!strcmp(argv[i],"inverted")) { rotations |= inverted; r_opts--;}
+			if (!strcmp(argv[i],"right"))    { rotations |= right;    r_opts--;}
+		}
+	}
+	//setting default rotations
+	for (; r_opts > 0; r_opts--) {
+		rotations |= 1<<(r_opts-1);
+	}
 
-	display = XOpenDisplay(display_name);
-	screen = DefaultScreen(display);
-	root_win = RootWindow(display,screen);
-	screen_config = XRRGetScreenInfo(display,root_win);
+	xhead.display = XOpenDisplay(disp_name);
+	xhead.screen  = DefaultScreen(xhead.display);
+	xhead.root    = RootWindow(xhead.display,xhead.screen);
 
-	current_rate = XRRConfigCurrentRate(screen_config);
-	size = XRRConfigCurrentConfiguration(screen_config,&current_rotation);
+	xrr.screen_config = XRRGetScreenInfo(xhead.display,xhead.root);
+	xrr.size = XRRConfigCurrentConfiguration(xrr.screen_config,&xrr.rotation);
 
-	printf("Rotation: %d\n",current_rotation);
-	printf("Size: %d\n",size);
-	printf("Rate: %d\n",current_rate);
+	if ( !(xrr.rotation & rotations & 0xff) ) { //current orientation sucks :)
+		if (opts.force) {
+			for (i = 4; i >= 0; i--) 
+				if ( 0xff & (xrr.rotation = rotations & (1<<i)) ) break;
+		}
+		else {
+			fprintf(stderr, "current display rotation is not in given rotations\n");
+			fprintf(stderr, "use -f option, if you want to force switching\n");
+			return 1;
+		}
+	}
 
+	//switching rotation
+	xrr.rotation ^= rotations;
+
+	return XRRSetScreenConfig(xhead.display,xrr.screen_config,xhead.root,
+							  xrr.size, xrr.rotation,CurrentTime);
 
 }
 
